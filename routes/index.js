ce7634b5f342341e331405ad824fba0627b99e7e
naynek <naynkc15@gmail.com>
2015-07-14 18:28:02 -0500

    Multiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
 router.get('/author',quizController.author);
+
+router.param('quizId',quizController.load); // autoload :quizId
+// Definicion de ruas de /quizes
+router.get('/quizes',quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+           
 
 module.exports = router;
