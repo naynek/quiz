b28f2813623d6aafaa64babaa8a070b7fdf29be0
naynek <naynkc15@gmail.com>
2015-07-15 18:23:03 -0500

    Crear comentarios

--- models/models.js
+++ models/models.js
@@ -27,10 +27,21 @@ var sequelize = new Sequelize(DB_name,user,pwd,
                                 omitNull:true // solo postgres
                               }
                               );
-// Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-exports.Quiz = Quiz; // exportar la definicion de la tabla Quiz
+
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definicion de la tabla comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz;
+exports.Comment =  Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en BD
 sequelize.sync().then(function(){
@@ -49,3 +60,4 @@ sequelize.sync().then(function(){
         };
     });
 });
+
